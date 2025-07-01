0698acb08bc91a6cae289a9a6620f136f6e4cb16
hamza <naqshband.hamza@gmail.com>
2025-07-01 09:15:55 +0500

    changes

--- src/app/(onboarding)/sign-in/page.tsx
+++ src/app/(onboarding)/sign-in/page.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
 import HomeNavbar from "@/components/layout/homeNavbar";
+import { Roboto,Inter,Montserrat } from 'next/font/google'
+ 
+const inter = Montserrat({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 
 export default function SignIn() {
   return (
-    <div className="relative h-screen w-full bg-[#FFFFFF] overflow-hidden"
+    <div className={`relative h-screen w-full bg-[#FFFFFF] overflow-hidden ${inter.className}`}
     style={{background: "linear-gradient(90deg, #3B41C5 0%, #A981BB 49%, #FFC8A9 100%)"}}
     >
       <HomeNavbar/>
-      <h1 className="text-gray-700 text-center mt-[120px] mb-[60px] font-bold text-3xl">Sign In to Our Chat App</h1>
-      <div className="w-[420px] h-auto shadow-md mx-auto mt-[20px] text-gray-900  rounded overflow-hidden">
-        <input className="input-field" type="text" name="username" placeholder="Username" />
+      <h1 className={`text-white text-center mt-[20px] mb-[60px] font-bold text-3xl`}>Sign In to Our Chat App</h1>
+      <div className="w-[420px] h-auto shadow-md mx-auto mt-[20px] text-gray-900 rounded-3xl overflow-hidden bg-[#ffffff]">
+        <input className="input-field mt-[40px]" type="text" name="username" placeholder="Username" />
         <input className="input-field" type="password" name="password" placeholder="Password" />
-        <button className="submit-btn">Sign In</button>
+        <button className={`submit-btn bg-[#3B41C5] border-none`}>Sign In</button>
       </div>
     </div>
   );
--- src/app/(onboarding)/sign-up/page.tsx
+++ src/app/(onboarding)/sign-up/page.tsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
 import HomeNavbar from "@/components/layout/homeNavbar";
+import { Roboto,Inter,Montserrat } from 'next/font/google'
+ 
+const inter = Montserrat({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function SignUp() {
   return (
-    <div className="relative h-screen w-full bg-[#FFFFFF] overflow-hidden"
+    <div className={`relative h-screen w-full bg-[#FFFFFF] overflow-hidden ${inter.className}`}
     style={{background: "linear-gradient(90deg, #3B41C5 0%, #A981BB 49%, #FFC8A9 100%)"}}
     >
       <HomeNavbar/>
-      <h1 className="text-gray-700 text-center mt-[120px] mb-[60px] font-bold text-3xl">Sign Up to Our Chat App</h1>
-      <div className="w-[420px] h-auto shadow-md mx-auto mt-[20px] text-gray-900  rounded overflow-hidden">
+      <h1 className={`text-white text-center mt-[20px] mb-[60px] font-bold text-3xl`}>Sign Up to Our Chat App</h1>
+      <div className="w-[420px] h-auto shadow-md mx-auto mt-[20px] text-gray-900  rounded-3xl overflow-hidden  bg-[#ffffff]">
         <input className="input-field mt-[40px]" type="text" name="name" placeholder="Full Name" />
         <input className="input-field" type="text" name="username" placeholder="Username" />
         <input className="input-field" type="email" name="email" placeholder="email@email.com" />
         <input className="input-field" type="password" name="password" placeholder="Password" />
         <input className="input-field" type="password" name="confirm-password" placeholder="Confirm Password" />
-        <button className="submit-btn">Sign Up</button>
+        <button className={`submit-btn bg-[#3B41C5] border-none`}>Sign Up</button>
       </div>
     </div>
   );
